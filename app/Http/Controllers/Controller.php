59138bfe93b4f0f9cbcecc55355fa4836c5e3ff0
akihiro.kawasaki <akihiro.kawasaki@rakuten.com>
2018-07-09 04:44:54 +0000

    user-follow

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,20 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+//カウントたしたちょ　（あき）
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function counts($user) {
+     //   $count_microposts = $user->microposts()->count(); himma listが多分入る　(あき)
+        $count_followings = $user->followings()->count();
+        $count_followers = $user->followers()->count();
+
+        return [
+          //  'count_microposts' => $count_microposts,　himmalistが多分はいる　(あき)
+            'count_followings' => $count_followings,
+            'count_followers' => $count_followers,
+        ];
+    }
 }
